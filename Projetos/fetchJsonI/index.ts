7241ad8ca569fccbe63d1578173b157b0ca3eecb
leonardolombardi07 <leonardolombardi07@hotmail.com>
2020-04-24 16:19:30 -0300

    Interface and Types done - Chapter I done

--- Projetos/fetchJsonI/index.ts
+++ Projetos/fetchJsonI/index.ts
@@ -10,12 +10,17 @@ interface Todo {
 
 axios.get(url).then((response) => {
   const todo = response.data as Todo;
-  const ID = todo.id;
+
+  const id = todo.id;
   const title = todo.title;
-  const finished = todo.completed;
-  console.log(`
-  The TODO with ID: ${ID}
-  Has a title of: ${title}
-  Is it finished? ${finished}
-  `);
+  const completed = todo.completed;
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+  The TODO with ID: ${id}
+  Has a title of: ${title}
+  Is it finished? ${completed}
+  `);
+};
